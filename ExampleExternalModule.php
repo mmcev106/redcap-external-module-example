25f5579b8f094d79c167ce8cb83a46f47d5c7a12
Mark McEver <mark@fastmail.net>
2016-11-15 08:12:11 -0600

    Moved the example module out of the https://github.com/kevstl/REDCap_Modules.git repo

---
+++ ExampleExternalModule.php
@@ -0,0 +1,18 @@
+<?php
+namespace ExternalModules;
+require_once dirname(__FILE__) . '/../../classes/ExternalModules.php';
+
+class ExampleExternalModule extends AbstractExternalModule
+{
+	function hook_project_home_page($project_id)
+	{
+		$this->selectData('...');
+		$this->updateUserPermissions('...');
+
+		?>
+		<script>
+			console.log("This message came from a hook defined in the ExampleExternalModule!  Your project id is <?= $project_id ?>!");
+		</script>
+		<?php
+	}
+}
---
+++ project_homepage.php
@@ -0,0 +1,17 @@
+<?php
+namespace ExternalModules;
+require_once __DIR__ . '/../../classes/ExternalModules.php';
+require_once ExternalModules::getProjectHeaderPath();
+require_once 'ExampleExternalModule.php';
+
+
+$exampleModule = new ExampleExternalModule();
+
+?>
+
+<h1>This is the Example Module project homepage!</h1>
+<p>Here is some data from the database: <?= $exampleModule->selectData('...') ?><p>
+
+<?php
+
+require_once ExternalModules::getProjectFooterPath();
