dbf3d0b320fb802f11aca4063b29b52e7fb8a03b
oriapp <VICTR.Server.Admins@vanderbilt.edu>
2017-08-08 11:07:03 -0500

    Added a namespace specific to this module

--- ExampleExternalModule.php
+++ ExampleExternalModule.php
@@ -1,5 +1,8 @@
 <?php
-namespace ExternalModules;
+namespace Vanderbilt\ExampleExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
 
 class ExampleExternalModule extends AbstractExternalModule
 {
